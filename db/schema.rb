4de43946f43000af94588d5e83f91c21fb1238bb
hannahhiraki <hmh1901@iolani.org>
2014-04-19 02:31:26 +0000

    work with db

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20140419011911) do
   enable_extension "plpgsql"
 
   create_table "posts", force: true do |t|
-    t.string   "tile"
+    t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
